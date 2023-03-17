07685127a918606297c2a9256c8cdce4922a90ab
mrclench <charlesnnnortey@gmail.com>
2023-03-17 17:05:30 +0000

    line errors

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	//string initialisation
-	char Mystr[] = ""Programming is like building a multilingual puzzle"/";
-	puts(Mystr); //writing the string to stdout
+	puts("Programming is like building a multilingual puzzle"/");
 	return (0);
 }
