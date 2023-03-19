d4242e29a34e495e9893c2837f46a59a6073d940
mrclench <charlesnnnortey@gmail.com>
2023-03-19 22:22:35 +0000

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -5,25 +7,17 @@
  */
 int main(void)
 {
-    int i, j;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            putchar(i + '0');
-            putchar(',');
-            putchar(' ');
-            putchar(j + '0');
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{	putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
 }
-
