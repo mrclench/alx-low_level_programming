839612fbe97042b1a755ca5cfb397e66f8a893bf
mrclench <charlesnnnortey@gmail.com>
2023-03-21 01:06:52 +0000

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,7 +23,7 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if (i + k + k != 24)
+					if (i + j + k != 24)
 					{
 						putchar(',');
 						putchar(' ');
