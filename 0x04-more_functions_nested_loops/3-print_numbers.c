2ea08eefaab99b6be194480d1343bdaf277f33a3
mrclench <charlesnnnortey@gmail.com>
2023-03-23 23:20:05 +0000

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
