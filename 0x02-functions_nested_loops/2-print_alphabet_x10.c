1fafd07c9bcebfb89f6859af883fb032a2740dce
mrclench <charlesnnnortey@gmail.com>
2023-03-28 18:51:46 +0000

    t

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,8 +12,9 @@ void print_alphabet_x10(void)
 
 	for (i = 1 ; i <= 10 ; i++)
 	{
-		for (j = 'a' ; j <= 'z' ; j++)
-			_putchar(j);
+		_putchar(j)
+			for (j = 'a' ; j <= 'z' ; j++)
+				_putchar(j);
 		_putchar('\n');
 	}
 }
