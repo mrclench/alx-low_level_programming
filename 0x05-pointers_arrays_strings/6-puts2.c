3e6963b707bc0ff5d4e3409e5a35da220ff389b5
mrclench <charlesnnnortey@gmail.com>
2023-03-28 15:56:30 +0000

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other number
+ *
+ * @str: parameter used in function
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i += 2)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
