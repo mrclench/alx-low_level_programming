da3371f83e8a48e1da0da74e9cfd17459ccf7734
mrclench <charlesnnnortey@gmail.com>
2023-03-28 18:19:46 +0000

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10 times the numbers
+ *
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int h;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
