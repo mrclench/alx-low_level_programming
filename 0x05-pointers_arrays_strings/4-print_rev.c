c6dcac7447de80abc0891a6320cfbfd0352dc0ab
mrclench <charlesnnnortey@gmail.com>
2023-03-28 01:54:02 +0000

    f

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - reverse a string
+ *
+ * @s: string to print
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = count ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
