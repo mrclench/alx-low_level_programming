80312159978506c83c69c2899d6056a3f2df2833
mrclench <charlesnnnortey@gmail.com>
2023-04-15 15:37:59 +0000

    error with betty will recheck

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings with wildcards in the second 
+ * char * - strings to compare
+ * @s1: first string
+ * @s2: second string
+ * Return:  returns 1 if the strings are identical, otherwise return 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		while (*s2 == '*')
+			s2++;
+
+		if (*s2 == '\0')
+			return (1);
+
+		while (*s1 != '\0')
+		{
+			if (wildcmp(s1, s2))
+				return (1);
+			s1++;
+		}
+
+	
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	return (0);
+}
