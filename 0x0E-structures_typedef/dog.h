ccbe01f57e8265ff812c526afdb4ff199fdb8c16
mrclench <charlesnnnortey@gmail.com>
2023-04-18 22:38:57 +0000

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ *
+ * Description: Longer escription
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
