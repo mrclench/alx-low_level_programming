14f776d60a7720d34b975835cbc468f736711fc9
mrclench <charlesnnnortey@gmail.com>
2023-04-29 22:39:19 +0000

    fixed

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,10 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "3-calc.h"
 /**
- * get_op_func - this function selects the correct function
- * @s: operator passed as argument
- *
- * Return:  pointer to the function that corresponds
+ * get_op_func - gets the right func
+ * @s: function s
+ * Return: int value
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -18,14 +16,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (i < 5)
+	i = 0;
+	while (ops[i].f != NULL)
 	{
-		if (*ops[i].op == *s && !(*(s + 1)))
+		if (*s == *(ops[i].op) && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
-}
 
+	printf("Error\n");
+	exit(99);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,31 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "3-calc.h"
 /**
- * main - the main function of calculator
- * @argc: count arguments
- * @argv: array of line arguments
- * Return: 0
+ * main - main function of the program
+ * @argc: arguments
+ * @argv: array of args
+ * Return: return something
  */
 int main(int argc, char *argv[])
-
 {
-	int a, b;
-	int (*operation)(int, int);
-
-	if (argc != 4)
+	if (argc == 4)
 	{
-		printf("Error\n");
-		exit(98);
+		int a;
+		int b;
+		int (*func)(int, int);
+
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+		func = get_op_func(argv[2]);
+		if ((*argv[2] == '%' || *argv[2] == '/') && b == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+		printf("%d\n", func(a, b));
+		return (0);
 	}
-
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", operation(a, b));
-	return (0);
+	printf("Error\n");
+	exit(98);
 }
