8db75c235c86fa457b91560835e052aeeeb947ab
mrclench <charlesnnnortey@gmail.com>
2023-05-02 18:31:06 +0000

    fixed

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_listint - lists sum of listint list
+ *
+ * @head: head of list
+ *
+ * Return: value of sum of list
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	for (sum = 0; head != NULL; head = head->next)
+		sum += head->n;
+
+	return (sum);
