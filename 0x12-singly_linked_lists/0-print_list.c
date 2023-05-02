1d730b5061e3603053fe8654c34c99d15823fd4e
mrclench <charlesnnnortey@gmail.com>
2023-05-02 16:58:07 +0000

    check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,21 +10,21 @@
 
 size_t print_list(const list_t *h)
 {
-        int count = 0;
+	int count = 0;
 
-        while (h != NULL)
-        {
-                if (h->str == NULL)
-                {
-                        printf("[%d] %s\n", 0, "(nil)");
-                        h = h->next;
-                        count++;
-                        continue;
-                }
-                printf("[%d] %s\n", h->len, h->str);
-                h = h->next;
-                count++;
-        }
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			h = h->next;
+			count++;
+			continue;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
 
-        return (count);
+	return (count);
 }
