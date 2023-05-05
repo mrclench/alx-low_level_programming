470520553a4a461e381e93785ebc057e6452f19e
mrclench <charlesnnnortey@gmail.com>
2023-05-05 10:54:30 +0000

    r

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,23 +11,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 1;
 	unsigned int i = 0;
-	int c;
+	unsigned int c;
 	unsigned int len;
-
-	len = strlen(b);
 	if (b == NULL)
-		return (0);
-	for (c = len - 1; c >= 0; c--)
+		return 0;
+	len = strlen(b);
+	for (c = 0; c < len; c++)
 	{
 		if (b[c] != '0' && b[c] != '1')
-			return (0);
-		if (b[c] == '1')
-		{
-			i += k;
-		}
-		k *= 2;
+			return 0;
+	i <<= 1;
+	i |= (b[c] - '0');
 	}
 	return (i);
 }
