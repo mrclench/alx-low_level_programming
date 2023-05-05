7429db5a8addbb574e43c9d1493e2452178da447
mrclench <charlesnnnortey@gmail.com>
2023-05-05 10:58:04 +0000

    redone

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,13 +14,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i = 0;
 	unsigned int c;
 	unsigned int len;
+
 	if (b == NULL)
-		return 0;
+	{
+		return (0);
+	}
 	len = strlen(b);
 	for (c = 0; c < len; c++)
 	{
 		if (b[c] != '0' && b[c] != '1')
-			return 0;
+			return (0);
 	i <<= 1;
 	i |= (b[c] - '0');
 	}
