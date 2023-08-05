dfeecea2807a309641d5b81eeb51a6b02aa4965d
mrclench <charlesnnnortey@gmail.com>
2023-08-04 18:49:20 -0700

    kiki

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-get_bit.c"
+/**
+ * clear_bit - clears index and sets bit to 0
+ * @n: integer to look out for
+ * @index: index to set to
+ * Return: returns 1 if success and -1 if invalid
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+
+	(*n) &= ~(1 << index);
+
+	if (get_bit((*n), index) == 0)
+		return (1);
+
+	return (-1);
+}
