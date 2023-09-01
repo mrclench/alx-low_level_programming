b6ac7e93cfabfa45505dea8958e87245017bd15a
mrclench <charlesnnnortey@gmail.com>
2023-08-31 20:57:52 -0700

    f

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-get_bit.c"
+
+/**
+ * set_bit - sets a bit to 1
+ * @n: integer to use
+ * @index: index to set to 1
+ *
+ * Return: 1 if succeeded of -1 if it failed
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 32)
+		return (-1);
+
+	(*n) |= 1 << index;
+	if (get_bit((*n), index) == 1)
+		return (1);
+	return (-1);
+}
+
