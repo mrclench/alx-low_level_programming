6e044ebd04126b9d6b792fae04c286afd8abe39c
mrclench <charlesnnnortey@gmail.com>
2023-09-04 09:20:14 -0700

    check Hello Word

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,30 +7,28 @@
 /**
  * append_text_to_file - appends text to the end of a file
  * @filename: the name of the file
- * @text_content: content added
+ * @text_content: content to be appended
  *
- * Return: 1 if the file already exists. -1 if it doest exist
+ * Return: 1 on success, -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, rwr;
 	size_t len = 0;
 
 	if (filename == NULL)
-		return (-1);
+	return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
-		return (-1);
+	return (-1);
 
 	if (text_content != NULL)
 	{
-		return (1);
-	for (len = 0; text_content[len]; len++)
+		for (len = 0; text_content[len]; len++)
 		;
-	rwr = write(fd, text_content, len);
+		rwr = write(fd, text_content, len);
 
 		if (rwr == -1)
 		{
