c9ea13dd803bf4b831ed85edff71fa874d0346fd
mrclench <charlesnnnortey@gmail.com>
2023-09-19 20:33:53 +0000

    corrected main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #define BUFSIZE 1024
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
