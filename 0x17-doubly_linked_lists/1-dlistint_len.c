e568efefdb011ee04bb29388d2d9b72400b25dd9
mrclench <charlesnnnortey@gmail.com>
2023-10-12 17:58:41 +0000

    worked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,19 @@
 #include "lists.h"
+
 /**
- *  dlistint_len - this function reads number of linked list
+ * dlistint_len - this function reads number of linked list
  *
- *  @h: head of linked list
+ * @h: head of linked list
  *
- *  Return: number of elements
+ * Return: number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *currentnode;
-	
+
 	size_t n;
-	
+
 	currentnode = h;
 	n = 0;
 	while (currentnode)
