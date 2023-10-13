d3d4485a4869b52efb3b008c42e6579eaf0408e2
mrclench <charlesnnnortey@gmail.com>
2023-10-13 03:15:17 +0000

    grr

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
  * get_dnodeint_at_index - function returns the nth node of a linked list
-
+ *
  * @head: Pointer to start of the list
-
+ *
  * @index: Index of node, starting from 0
  *
  * Return: Specified node of a linked list or NULL if node does not exist
@@ -25,3 +25,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	}
 	return (NULL);
 }
+
